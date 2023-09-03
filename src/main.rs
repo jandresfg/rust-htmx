ead72b4d946985e456964d64abd3ffd6b5bab93e
Andres Fernandez <jandresfgz@gmail.com>
2023-09-03 02:47:17 -0500

    feat: create 404.html and return it as the fallback endpoint

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,27 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let contents = fs::read_to_string("index.html").unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 404 NOT FOUND\r\nContent-length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
